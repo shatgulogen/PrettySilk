8050977b223de82a36cc40f873b1c6c752c15aca
Shatgul <shatgul@shatgul>
2022-04-19 03:38:46 -0400

    Created React App and frontend folder to have the initial run

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <a href="/">PrettySilk</a>
+            </header>
+            <main>list the pretty silk scarfs</main>
+        </div>
+    );
+}
+
+export default App;
