62b4aae42d7a6790b15559c6ae68f09f60d11092
Shatgul <shatgul@shatgul>
2022-04-19 05:01:58 -0400

    add frontend content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import data from './data';
+
 function App() {
     return (
         <div>
             <header>
                 <a href="/">PrettySilk</a>
             </header>
-            <main>list the pretty silk scarfs</main>
+            <main>
+                <h1>Featured Products</h1>
+                <div classNames="products">
+                    {data.products.map((product) => (
+                        <div className="product" key={product.slug}>
+                            <img src={product.image} alt={product.name} />
+                            <p>{product.name}</p>
+                            <p>{product.price}</p>
+                        </div>
+                    ))}
+                </div>
+            </main>
         </div>
     );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: 'Eloise',
+            slug: 'Eloise',
+            category: 'Scarfs',
+            image: '/images/1.png',
+            price: 59,
+            inventoryCount: 15,
+            brand: 'PrettySilk',
+            rating: 4.5,
+            numReviews: 10,
+            description:
+                'Elegant floral soft sea-foam green and pink 100% silk scarf with a measurement of 185cm * 15cm',
+        },
+        {
+            name: 'Alize',
+            slug: 'Alize',
+            category: 'Scarfs',
+            image: '/images/2.png',
+            price: 55,
+            inventoryCount: 10,
+            brand: 'PrettySilk',
+            rating: 4.7,
+            numReviews: 18,
+            description:
+                'Elegant floral coffee and cream tones 100% silk scarf with a measurement of 185cm * 15cm',
+        },
+        {
+            name: 'Amélie',
+            slug: 'Amélie',
+            category: 'Scarfs',
+            image: '/images/4.png',
+            price: 49,
+            inventoryCount: 10,
+            brand: 'PrettySilk',
+            rating: 5,
+            numReviews: 10,
+            description:
+                'Elegant soft cream 100% silk scarf with a measurement of 50cm * 50cm',
+        },
+        {
+            name: 'Elania',
+            slug: 'Elania',
+            category: 'Scarfs',
+            image: '/images/3.png',
+            price: 59,
+            inventoryCount: 19,
+            brand: 'PrettySilk',
+            rating: 4.9,
+            numReviews: 12,
+            description:
+                'Elegant floral soft blues 100% silk scarf with a measurement of 185cm * 15cm',
+        },
+    ],
+};
+
+export default data;
