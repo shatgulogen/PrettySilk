7a8fb624a7eb641487e1f3ccda4294255ee3533f
Shatgul <shatgul@shatgul>
2022-04-27 06:33:32 -0400

    clean up backend structure to include minimum for now and update signinpage

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -33,6 +33,8 @@ function reducer(state, action) {
             localStorage.setItem('cartItems', JSON.stringify(cartItems));
             return { ...state, cart: { ...state.cart, cartItems } };
         }
+        case 'USER_SIGNIN':
+            return { ...state, userInfo: action.payload };
         default:
             return state;
     }
--- frontend/src/pages/SigninPage.js
+++ frontend/src/pages/SigninPage.js
@@ -1,29 +1,64 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import { Helmet } from 'react-helmet-async';
 import Button from 'react-bootstrap/Button';
 import FormGroup from 'react-bootstrap/esm/FormGroup';
+import Axios from 'axios';
+import { useContext, useState } from 'react';
+import { Store } from '../Store';
 
 export default function SigninPage() {
     const { search } = useLocation();
+    const navigate = useNavigate();
     const redirectInUrl = new URLSearchParams(search).get('redirect');
     const redirect = redirectInUrl ? redirectInUrl : '/';
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { state, dispatch: contextDispatch } = useContext(Store);
+    const { userInfo } = state;
+
+    const submitHandler = async (e) => {
+        e.preventDefault(); //prevent refreshing this sign in page when user click on sign in button
+        try {
+            const { data } = await Axios.post('/api/users/signin', {
+                email,
+                password,
+            });
+            // console.log(data);
+            contextDispatch({ type: 'USER_SIGNIN', payload: data });
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            navigate(redirect || '/'); //just go to homepage if userInfo does not exist in database
+        } catch (err) {
+            alert('Invalid email or password');
+        }
+    };
+
     return (
         <Container className="small-container">
             <Helmet>
                 <title>Sign in</title>
             </Helmet>
             <h1 className="my-3">Sign In</h1>
-            <Form>
+            <Form onSubmit={submitHandler}>
                 <FormGroup className="mb-3" controlId="email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" required />
+                    <Form.Control
+                        type="email"
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </FormGroup>
 
                 <FormGroup className="mb-3" controlId="password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" required />
+                    <Form.Control
+                        type="password"
+                        required
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </FormGroup>
                 <div className="mb-3">
                     <Button type="submit">Sign In</Button>
